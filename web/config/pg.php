0109e2be3eb97b2886b590655a223e79030f0c17
bdegoy <consultants@degoy.com>
2019-05-02 12:12:53 +0200

    Réorganisation des sources

--- web/config/pg.php
+++
@@ -1,6 +0,0 @@
-<?php
-/* Connexion à la base de données PgSQL
-*/
-if (!defined("_ECRIRE_INC_VERSION")) return;
-spip_connect_db('localhost','5432','dnc_user','oidcinteX9BVqA8','dnc_oauth2','pg', 'oauth','','utf8');
- 
