febfff5920c4035d2842fe71354b4314b9dd0f40
bdegoy <consultants@degoy.com>
2019-06-03 08:36:51 +0200

    sendsms is working now

--- oidc/tests/sendsms.php
+++ oidc/tests/sendsms.php
@@ -1,9 +1,11 @@
 <?php
-  
-  
+
+
 // Autoloading by Composer
 require_once '../../vendor/autoload.php';
+// https://github.com/ovh/php-ovh
 use \Ovh\Api;
+// https://github.com/ovh/php-ovh-sms
 use \Ovh\Sms\SmsApi;
 
 $time_start = microtime(true);
@@ -28,7 +30,7 @@ sendsms('+33786822490','coucou');
 function sendsms($receiver, $message) {   
 
     //$message = substr($message, 0, 160);  // 160 chars max
-   
+
 
     // Init SmsApi object
     $Sms = new SmsApi( OVHSMSAPI_APPLICATIONKEY, OVHSMSAPI_APPLICATIONSECRET, OVHSMSAPI_ENDPOINT, OVHSMSAPI_CONSUMER_KEY );
@@ -37,21 +39,30 @@ function sendsms($receiver, $message) {
     $accounts = $Sms->getAccounts();
 
     if ( $accounts[0] ) {
-        
+
         // Set the account 
         $Sms->setAccount($accounts[0]);
         $senders = $Sms->getSenders();
         
-        // Create a new message that will allow the recipient to answer (to FR receipients only)
-        $Message = $Sms->createMessage(true);
-        $Message->setSender($senders[0]);
-        $Message->addReceiver($receiver);
-        $Message->setIsMarketing(false);
+        $details = $Sms->getUserDetails( OVHSMSAPIUSER );
+        if ( 
+        $details['quotaInformations']['quotaStatus'] === 'inactive' 
+        OR ($details['quotaInformations']['quotaStatus'] === 'active' AND $details['quotaInformations']['quotaLeft'] > 0 ) 
+        ) { 
+            
+            // Set user
+            $void = $Sms->setUser( OVHSMSAPIUSER );
+            // Create a new message that will allow the recipient to answer (to FR receipients only)
+            $Message = $Sms->createMessage();
+            $Message->setSender($senders[0]);
+            $Message->addReceiver($receiver);
+            $Message->setIsMarketing(false);
 
-        // Send it
-        $Message->send($message);
-        
-        return true;
+            // Send it
+            return $Message->send($message);
+
+        } else
+            return false;
 
     } else 
         return false;
